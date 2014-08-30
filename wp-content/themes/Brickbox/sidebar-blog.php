01bc73da3e0171273943e37e7258b3f1870ef0b5
Kieran Coughlan <kcoughlan78@me.com>
2014-08-30 16:48:47 +0100

    add new file and make initial changes to style.css

---
+++ wp-content/themes/Brickbox/sidebar-blog.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The Blog Sidebar containing the Blog widget area
+ *
+ * @package WordPress
+ * @subpackage Brickboox
+ * @since Brickbox 1.0
+ */
+?>
+<div id="secondary">
+	<?php
+		$description = get_bloginfo( 'description', 'display' );
+		if ( ! empty ( $description ) ) :
+	?>
+	<h2 class="site-description"><?php echo esc_html( $description ); ?></h2>
+	<?php endif; ?>
+
+	<?php if ( has_nav_menu( 'secondary' ) ) : ?>
+	<nav role="navigation" class="navigation site-navigation secondary-navigation">
+		<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
+	</nav>
+	<?php endif; ?>
+
+	<?php if ( is_active_sidebar( 'sidebar-4' ) ) : ?>
+	<div id="blog-sidebar" class="blog-sidebar widget-area" role="complementary">
+		<?php dynamic_sidebar( 'sidebar-4' ); ?>
+	</div><!-- #primary-sidebar -->
+	<?php endif; ?>
+</div><!-- #secondary -->
